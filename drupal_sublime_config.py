c458dd6cc1195368f53b39047ef32942949ce7fe
Love Huria <lhuria94@gmail.com>
2018-01-08 22:00:12 +0530

    New implementation added

--- drupal_sublime_config.py
+++ drupal_sublime_config.py
@@ -1,13 +1,20 @@
-import sublime, sublime_plugin, os, inspect
+import sublime, sublime_plugin, sys
+enc = sys.getdefaultencoding()
 
 # Get Sublime packages path dynamically
-parent_path = os.path.abspath(os.path.dirname(__file__))
+packages_path = sublime.packages_path()
 
 class DrupalSublimeConfigCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     # Copying drupal compatible config to Sublime user settings
     # Ref: https://www.drupal.org/docs/develop/development-tools/configuring-sublime-text
-    with open(os.path.dirname(os.path.abspath(inspect.stack()[0][1])) + "/DrupalCompatible.sublime-settings", 'r') as f:
-      with open(os.path.dirname(parent_path) + "/User/Preferences.sublime-settings", "w") as f1:
+    with open((packages_path) + "/DrupalSublimeConfig/DrupalCompatible.sublime-settings", 'r') as f:
+      with open((packages_path) + "/User/Preferences.sublime-settings", "w") as f1:
         for line in f:
           f1.write(line)
+
+class FcSampleTestStepsTemplate(sublime_plugin.TextCommand):
+  def run(self, edit):
+    with open((packages_path) + "/DrupalSublimeConfig/SampleTestStepsTemplate.txt", 'r', encoding=enc) as f:
+      content = f.read()
+      self.view.insert(edit, 0, content)
