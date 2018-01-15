4fe20aac56ac217aa63e7534f81f7324b4c697cd
Love Huria <lhuria94@gmail.com>
2018-01-15 12:04:04 +0530

    Optimized code

--- drupal_sublime_config.py
+++ drupal_sublime_config.py
@@ -1,6 +1,5 @@
 import sublime, sublime_plugin, sys, os
 import urllib
-from requests import get
 from io import BytesIO
 from zipfile import ZipFile
 from glob import glob
@@ -12,6 +11,19 @@ packages_path = sublime.packages_path()
 
 class DrupalSublimeConfigCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+    # PHPcs linter path
+    mat_url = 'https://github.com/equinusocio/material-theme/archive/master.zip'
+    # Download files from internet resources
+    urllib.request.urlretrieve(mat_url, packages_path + '/Material Theme.zip')
+    # Check if file exists
+    if os.path.isfile(packages_path + '/Material Theme.zip'):
+      zf = ZipFile(packages_path + '/Material Theme.zip')
+      zf.extractall(packages_path)
+      zf.close()
+      # Remove file after finishing operations
+      os.remove(packages_path + '/Material Theme.zip')
+      os.rename(packages_path + '/material-theme-master', packages_path + '/Material Theme')
+
     # Copying drupal compatible config to Sublime user settings
     # Ref: https://www.drupal.org/docs/develop/development-tools/configuring-sublime-text
     with open((packages_path) + "/DrupalSublimeConfig/DrupalCompatible.sublime-settings", 'r') as f:
@@ -29,13 +41,17 @@ class DownloadLinters(sublime_plugin.TextCommand):
   def run(self, edit):
     # PHPcs linter path
     phpcs_url = 'https://github.com/benmatselby/sublime-phpcs/archive/master.zip'
+    self._downloadInternetResources(phpcs_url, 'sublime-phpcs-master', 'Phpcs')
+
+  def _downloadInternetResources(self, path, folderName, replacement):
     # Download files from internet resources
-    urllib.request.urlretrieve(phpcs_url, packages_path + '/Phpcs.zip')
+    urllib.request.urlretrieve(path, packages_path + '/' + replacement + '.zip')
+
     # Check if file exists
-    if os.path.isfile(packages_path + '/Phpcs.zip'):
-      zf = ZipFile(packages_path + '/Phpcs.zip')
+    if os.path.isfile(packages_path + '/' + replacement + '.zip'):
+      zf = ZipFile(packages_path + '/' + replacement + '.zip')
       zf.extractall(packages_path)
       zf.close()
       # Remove file after finishing operations
-      os.remove(packages_path + '/Phpcs.zip')
-      os.rename(packages_path + '/sublime-phpcs-master', packages_path + '/Phpcs')
+      os.remove(packages_path + '/' + replacement + '.zip')
+      os.rename(packages_path + '/' + folderName, packages_path + '/' + replacement)
